6a7334804675b51bad4ca134d7ece27e6245c7d1
Meenakshi2000 <meenakshi.daisy@gmail.com>
2021-01-25 13:30:50 +0530

    added

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,7 +8,7 @@ class Database
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("127.0.0.1", "root", "", "store_db");
+		$this->con = new Mysqli("localhost", "root", "", "store_db");
 		return $this->con;
 	}
 }
