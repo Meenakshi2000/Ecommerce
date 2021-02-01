43725d69c45bd0af73170f19a2784290b3d4f524
Meenakshi2000 <meenakshi.daisy@gmail.com>
2021-02-01 15:20:57 +0530

    update

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,8 +8,8 @@ class Database
 	
 	private $con;
 	public function connect(){
-		// $this->con = new Mysqli("localhost", "root", "", "heroku_71dd34b7c3484d6");
-		$this->con =mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+		$this->con = new Mysqli("eu-cdbr-west-03.cleardb.net", "b7f63ab3755a58", "254492", "heroku_71dd34b7c3484d6");
+		// $this->con =mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 		return $this->con;
 
 
