ad1a5923cc5e828e1b0900afa9cb01668ad58638
Meenakshi2000 <meenakshi.daisy@gmail.com>
2021-02-01 14:38:11 +0530

    update

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -9,7 +9,10 @@ class Database
 	private $con;
 	public function connect(){
 		$this->con = new Mysqli("localhost", "root", "", "store_db");
+		$this->con =mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 		return $this->con;
+
+
 	}
 }
 
