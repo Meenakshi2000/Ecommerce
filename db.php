9b79049e6c45b4e5bb44eb757b8155127678896f
Meenakshi2000 <meenakshi.daisy@gmail.com>
2021-02-01 15:32:44 +0530

    update

--- db.php
+++ db.php
@@ -18,7 +18,7 @@
 
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("eu-cdbr-west-03.cleardb.net"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
