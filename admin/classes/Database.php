89ce7d709c2dea47c0dae8fd10b33fd97329c93a
Meenakshi2000 <meenakshi.daisy@gmail.com>
2021-02-01 14:55:30 +0530

    update

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,7 +8,7 @@ class Database
 	
 	private $con;
 	public function connect(){
-		// $this->con = new Mysqli("localhost", "root", "", "store_db");
+		// $this->con = new Mysqli("localhost", "root", "", "heroku_71dd34b7c3484d6");
 		$this->con =mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 		return $this->con;
 
--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DATABASE_NAME', 'store_db');
+define('HOST', 'eu-cdbr-west-03.cleardb.net');
+define('USER', 'b7f63ab3755a58');
+define('PASSWORD', '254492');
+define('DATABASE_NAME', 'heroku_71dd34b7c3484d6');
 
 define('CURRENCY', '€');
 
